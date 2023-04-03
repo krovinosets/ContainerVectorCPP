43cdf1ab314e7615a452a51c681b497c36996d70
BuildTools <unconfigured@null.spigotmc.org>
2023-04-04 01:32:33 +0300

    update

--- m_vector.h
+++ m_vector.h
@@ -1,8 +1,15 @@
 #ifndef M_VECTOR_H
 #define M_VECTOR_H
 
-
+#include <stdexcept>
 #include <initializer_list>
+
+class m_vectorException: public std::runtime_error
+{
+public:
+    explicit m_vectorException(const char* message) : std::runtime_error(message){}
+};
+
 template<typename Type>
 class m_vector
 {
@@ -18,16 +25,62 @@ public:
     m_vector<Type> &operator =(const m_vector<Type>& lst); // Присваивание вектора другому вектору
     Type &operator[](int index); // получить элемент как массив a[1]
     int get_length() const;
+    void set_elem(int index, const Type &elem);
+    Type &get_elem(int index);
+    //Type[] to_array();
+    m_vector<Type>& operator+=(const m_vector<Type>& vect);
 };
 
 template<typename Type>
-int m_vector<Type>::get_length() const{
+m_vector<Type> &m_vector<Type>::operator+=(const m_vector<Type> &vect){
+    m_vector<Type> local = *this;
+    amount = local.amount + vect.amount;
+    m_vec = new Type[amount]{};
+    for(int i = 0; i < local.amount; i++)
+        m_vec[i] = local.m_vec[i];
+    for(int i = 0; i < vect.amount; i++)
+        m_vec[local.amount+i] = vect.m_vec[i];
+    return *this;
+}
+
+//template<typename Type>
+//friend std::ostream& m_vector<Type>::operator<<(std::ostream& os, const m_vector<Type>&lst){
+
+//}
+
+//template<typename Type>
+//Type[] m_vector<Type>::to_array()
+//{
+
+//}
+
+template<typename Type>
+Type &m_vector<Type>::get_elem(int index)
+{
+    if(index < 0 || amount < index)
+        throw m_vectorException("Wrong index of m_vector");
+    return m_vec[index];
+}
+
+template<typename Type>
+void m_vector<Type>::set_elem(int index, const Type &elem)
+{
+    if(index < 0 || amount < index)
+        throw m_vectorException("Wrong index of m_vector");
+    m_vec[index] = elem;
+}
+
+template<typename Type>
+int m_vector<Type>::get_length() const
+{
     return amount;
 }
 
 template<typename Type>
 m_vector<Type>::m_vector(int length) : amount(length)
 {
+    if(length <= 0)
+        throw m_vectorException("Bad length of m_vector");
     m_vec = new Type[length]{};
 }
 
@@ -52,6 +105,8 @@ m_vector<Type>::m_vector(m_vector<Type> &&vect)
 template<typename Type>
 m_vector<Type>::m_vector(std::initializer_list<Type> lst) : amount(lst.size())
 {
+    if(amount <= 0)
+        throw m_vectorException("Bad length of m_vector");
     m_vec = new Type[amount]{};
     int i = 0;
     for(Type item : lst)
@@ -78,8 +133,9 @@ m_vector<Type> &m_vector<Type>::operator=(const m_vector<Type> &lst)
 template<typename Type>
 Type &m_vector<Type>::operator[](int index)
 {
+    if(index < 0 || amount < index)
+        throw m_vectorException("Wrong index of m_vector");
     return *(m_vec + index);
-    int a; // udali potom
 }
 
 #endif // M_VECTOR_H
--- main.cpp
+++ main.cpp
@@ -3,37 +3,112 @@
 
 using namespace std;
 
+void test_set_elem();
+void test_constructor();
+void test_copy();
+void test_transfer();
+void test_init_list();
+void test_get_elem();
+void test_operator_eq();
+
+template<typename Type>
+void print_m_vector(const char *message, m_vector<Type> vect){
+    printf("\n[%s]\n", message);
+    for(int i = 0; i < vect.get_length(); i++)
+        printf("| m_vector[%d] = %d\n", i, vect[i]);
+    printf("[END]\n");
+}
+
 int main()
 {
     cout << "Hello World!" << endl;
+    test_constructor();
+    test_copy();
+    test_transfer();
+    test_init_list();
+    test_set_elem();
+    test_get_elem();
+    test_operator_eq();
 
-    int len = 2;
-
-    m_vector<int> a(len);
-    m_vector<int> a2 = a;
-    m_vector<int> a3 = std::move(a2);
-    cout << "len: " << a.get_length() << endl;
-    for(int i = 0; i < len; i++)
-    {
-        printf("1 %d\n", a[i]);
-    }
-    cout << "len: " << a3.get_length() << endl;
-    for(int i = 0; i < len; i++)
-    {
-        printf("2 %d\n", a3[i]);
-    }
-    a2 = a3;
-    cout << "len: " << a2.get_length() << endl;
-    for(int i = 0; i < len; i++)
-    {
-        printf("3 %d\n", a2[i]);
-    }
-
-    m_vector<int> a4{0,1,2,3};
-    cout << "len: " << a4.get_length() << endl;
-    for(int i = 0; i < 4; i++)
-    {
-        printf("4 %d\n", a4[i]);
-    }
     return 0;
+}
+
+void test_operator_eq()
+{
+    try {
+        m_vector<int> a{4,5,6,3};
+        m_vector<int> a1{1, 1, 1};
+        a += a1;
+        print_m_vector("m_vector[operator_eq a] Print", a);
+    } catch(m_vectorException &e) {
+        cout << "Exception says: " << e.what() << endl;
+    }
+}
+
+void test_get_elem()
+{
+    try {
+        m_vector<int> a{4,5,6,3};
+        print_m_vector("m_vector[get elem a] Print", a);
+        int get_elem = a.get_elem(2);
+        printf("get_elem: %d\n", get_elem);
+        get_elem = a.get_elem(-2);
+        printf("get_elem: %d\n", get_elem);
+    } catch(m_vectorException &e) {
+        cout << "Exception says: " << e.what() << endl;
+    }
+}
+
+void test_init_list()
+{
+    m_vector<int> a{0,1,2,3};
+    print_m_vector("m_vector[init list a] Print", a);
+}
+
+void test_transfer()
+{
+    try{
+        m_vector<int> a(2);
+        m_vector<int> a2 = std::move(a);
+        print_m_vector("m_vector[Moved a2] Print", a2);
+    } catch(m_vectorException &e) {
+        cout << "Exception says: " << e.what() << endl;
+    }
+}
+
+void test_copy()
+{
+    try{
+        m_vector<int> a(2);
+        m_vector<int> a2 = a;
+        print_m_vector("m_vector[Copied a] Print", a2);
+    } catch(m_vectorException &e) {
+        cout << "Exception says: " << e.what() << endl;
+    }
+}
+
+void test_constructor()
+{
+    try {
+        m_vector<int> a(2);
+        print_m_vector("m_vector[a] Print", a);
+        m_vector<int> a1(-1);
+        m_vector<int> a2(0); // не доходит до сюда
+    } catch(m_vectorException &e) {
+        cout << "Exception says: " << e.what() << endl;
+    }
+}
+
+void test_set_elem()
+{
+    try {
+        m_vector<int> a(5);
+        print_m_vector("m_vector[set_elem_test] Print",a);
+        a.set_elem(3, 45);
+        print_m_vector("m_vector[set_elem_test] Print",a);
+        a.set_elem(99, 45);
+        print_m_vector("m_vector[set_elem_test] Print",a);
+    } catch(m_vectorException &e) {
+        cout << "Exception says: " << e.what() << endl;
+    }
 }
